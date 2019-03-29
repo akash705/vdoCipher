cd005b143577bc216a2c9e424a9ec33985294ce4
Akash mishra <mishraakash273694@gmail.com>
2019-03-29 13:42:23 +0530

    refining App.js

--- src/App.js
+++ src/App.js
@@ -12,25 +12,27 @@ import './App.css';
 class App extends Component {
   x = true;
   componentDidMount(){
-      requestGet({
-        url:requestUrl,config:{
-        headers:{
-          Authorization:`Bearer ${token}`
-          }
-        } 
-      })
-      .then(data=>{
-        if(data && data.data){
-          this.props.dispatchEvent({dataPayload:data.data});
-        }else{
-          this.props.dispatchEvent({error:true});
+    this.fetchImages();      
+  }
+  fetchImages=()=>{
+    requestGet({
+      url:requestUrl,config:{
+      headers:{
+        Authorization:`Bearer ${token}`
         }
-      })
-      .catch(data=>{
-        console.log(data);
-        this.props.dispatchEvent({error:"Network Error Couldn't reach Server"});
-      })
-      
+      } 
+    })
+    .then(data=>{
+      if(data && data.data){
+        this.props.dispatchEvent({dataPayload:data.data});
+      }else{
+        this.props.dispatchEvent({error:true});
+      }
+    })
+    .catch(data=>{
+      console.log(data);
+      this.props.dispatchEvent({error:"Network Error Couldn't reach Server"});
+    })
   }
   render() {
     return ( 
