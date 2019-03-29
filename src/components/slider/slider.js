bd651180bbba20a483608a054fa1b8e41403b3b9
Akash mishra <mishraakash273694@gmail.com>
2019-03-29 11:33:23 +0530

    Slider Set For Usage

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -2,12 +2,20 @@ import React,{Component} from 'react';
 
 import './slider.css';
 import img1 from './../../assets/images/img.jpeg';
+import img2 from './../../assets/images/image2.jpg';
+
 import Hoc from './../hoc/hoc';
 
 export class slider extends Component{
     state={
         activeSlide:0,
-        slider:[1,2]
+        slider:[{
+                data:1,
+                img:img1,
+            },{
+                data:2,
+                img:img2
+            }]
     }
     nextSlide=()=>{
         this.setState((state)=>{
@@ -33,19 +41,15 @@ export class slider extends Component{
         return (
             <Hoc>
                             <div className="slideshow-container">
-                     
                                 {
                                     this.state.slider.map((data,index)=>{
                                         return (
                                             <div key={index} className={(this.state.activeSlide==index)?"mySlides fade active":"mySlides fade"}>
-                                                <img src={img1} alt="no text"/>
+                                                <img src={data.img} alt="no text" className="slider-img"/>
                                             </div>
                                         )
                                     })
                                 }
-
-
-                                {/* onclick="plusSlides(-1)" */}
                             <a className="prev" href="#" onClick={ this.prevSlide } >&#10094;</a>
                             <a className="next" href="#" onClick={ this.nextSlide }>&#10095;</a>
                             {/*  onclick="plusSlides(1)" */}
